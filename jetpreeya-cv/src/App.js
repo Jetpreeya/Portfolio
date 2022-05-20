6645231e77352f9cf58d8e26f785662a1686fe76
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-05-20 17:08:35 +0200

    start to do portfolio

---
+++ jetpreeya-cv/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/navbar/navbar'
+import About from './components/about/about'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
---
+++ jetpreeya-cv/src/components/navbar/navbar.js
@@ -0,0 +1,19 @@
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="TopNav">
+    <nav className="navbar">
+      <ul className="ulNav">
+        <li>Home</li>
+        <li>About me</li>
+        <li>Projects</li>
+        <li>Contract me</li>
+      </ul>
+    </nav>
+    </div>
+
+  );
+};
+
+export default Navbar;
