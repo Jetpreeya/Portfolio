813e836813cfd08bd732f6cde3ee6f2bb11d4d59
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-05-25 16:44:47 +0200

    update at App.js and contract component

--- jetpreeya-cv/src/App.js
+++ jetpreeya-cv/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/navbar'
 import About from './components/about/about'
+import Contract from './components/contract/contract';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <About/>
+      <Contract/>
     </div>
   );
 }
--- jetpreeya-cv/src/components/navbar/navbar.js
+++ jetpreeya-cv/src/components/navbar/navbar.js
@@ -3,14 +3,14 @@ import "./navbar.css";
 const Navbar = () => {
   return (
     <div className="TopNav">
-    <nav className="navbar">
-      <ul className="ulNav">
-        <li>Home</li>
-        <li>About me</li>
-        <li>Projects</li>
-        <li>Contract me</li>
-      </ul>
-    </nav>
+      <nav className="navbar">
+        <ul className="ulNav">
+          <li className="home"><a href="/About"> Home</a></li>
+          <li>About me</li>
+          <li>Projects</li>
+          <li>Contract me</li>
+        </ul>
+      </nav>
     </div>
 
   );
