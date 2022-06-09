ce72a9c6ccfa2cc9697897699d55cca2f8653036
Jetpreeya <jetpreeya.norkaew@gmail.com>
2022-06-09 12:58:17 +0200

    Created Experience part

--- jetpreeya-cv/src/components/aboutMe/aboutMe.js
+++ jetpreeya-cv/src/components/aboutMe/aboutMe.js
@@ -3,9 +3,24 @@ import "./aboutMe.css";
 const AboutMe = () => {
   return (
     <div className="AboutMe">
-        <div className="Education">
+      <div className="Education">
         <h2>Education</h2>
-        </div>
+      </div>
+      <div className="Education-list">
+        <ul>
+          <li><h3>Programmer course .NET C#. </h3>
+            <p>April 2022 - Now : Lexicon Malmö, Sweden.</p></li>
+          <li><h3>Fullslack web developer intensive course.</h3>
+            <p>August 2021- March 2022 : FooCoding Malmö, Sweden.</p></li>
+          <li> <h3>Bachelor Degree of Economics.</h3>
+            <p>May 2013- December 2016 : Chiang Mai University, Thailand.</p></li>
+        </ul>
+      </div>
+
+      <div className="Experience">
+        <h2>Experience</h2>
+      </div>
+
     </div>
 
   );
