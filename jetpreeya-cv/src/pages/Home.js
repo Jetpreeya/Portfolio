d8a513efdf657a8c3ace000622f1ee574abdd1b8
Jetpreeya <jetpreeya.norkaew@gmail.com>
2022-07-29 10:26:22 +0200

    Add profile picture

--- jetpreeya-cv/src/pages/Home.js
+++ jetpreeya-cv/src/pages/Home.js
@@ -2,17 +2,18 @@ import React from "react";
 import "../styles/Home.css";
 import {FaGithub} from "react-icons/fa";
 import {AiFillLinkedin } from "react-icons/ai";
+import pic from '../assets/pic.jpg'
 
 
 function Home(){
     return (
     <div className="home">
         <div className="about">
-            <img src="https://avatars.githubusercontent.com/u/84719740?v=4" width="320" height="320"></img>
+            <img src={pic} alt="myPic" width="320" height="320"/>
             <h2>Hi, My Name is Jetpreeya</h2>
             <div className="prompt">
                 <p>Junior front-end developer</p>
-                <a href="https://www.linkedin.com/in/jetpreeya-yawichai/"  target="_blank"><AiFillLinkedin /></a>
+                <a href="https://www.linkedin.com/in/jetpreeya-yawichai/" target="_blank"><AiFillLinkedin /></a>
                 <a href="https://github.com/Jetpreeya"><FaGithub/></a>
             </div>
         </div>
